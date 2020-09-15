9460c55917e0751baaef8c2c6362ba9d8596623b
admin <smartreu@smartreu.co.uk>
2020-09-15 09:49:41 +0000

    Genesis

---
+++ assets/config.js
@@ -0,0 +1,39 @@
+// cpanel - site_templates/countdown_fresh/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "contact@smartreuse.co.uk",
+        logo: "Smart Reuse Lancaster",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/smartreuselancaster"
+            },
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "\/assets\/images\/chair1.jpg",
+            backgroundColor: "",
+            color: "",
+            buttonText: "Notify Me",
+            buttonLink: "",
+            endTime: '2020-09-30T11:00:00.000Z',
+            content: "Countdown to launch",
+        }
+    ]
+};
